367e9a5bcd8b919134d8439757ef86fbe3eaddb3
XD_CZ <mandlemankiller@email.cz>
2022-01-03 17:47:10 +0100

    First commit

---
+++ src/cz/jeme/consolebroadcast/ConsoleBroadcast.java
@@ -0,0 +1,52 @@
+package cz.jeme.consolebroadcast;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandException;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ConsoleBroadcast extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("console")) {
+			if (args.length < 2) {
+				sender.sendMessage("This command needs 2 arguments! (Usage: /console <recipient> <message>)");
+			} else {
+				String recipient = args[0];
+				StringBuffer message = new StringBuffer(30);
+				for (int i = 1; i < args.length; i++) {
+					message.append(args[i]);
+					if (i != args.length - 1) {
+						message.append(" ");
+					}
+				}
+				try {
+					Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + recipient
+							+ " [\"\",{\"text\":\"[\"},{\"text\":\"Console\",\"color\":\"#FF00E7\"},{\"text\":\"] Console: "
+							+ message.toString() + "\"}]");
+				} catch (CommandException e) {
+					sender.sendMessage("This command threw exeption, you probably wrote recipent wrongly. (Usage: /console <recipient> <message>)");
+				}
+
+			}
+			return true;
+		}
+		return false;
+
+	}
+
+	public void registerEvents() {
+
+	}
+
+}
